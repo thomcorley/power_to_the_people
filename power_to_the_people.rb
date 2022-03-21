00baa25a6326fb39b14e2fd0750b530a3981781c
Tom Corley <thomcorley@gmail.com>
2022-03-21 20:03:33 +0000

    Add parent module and a presenter class
    Calculator class and Presenter class now belong under the
    PowerToThePeople module. Presenter handles the displaying of the
    output, defaulting to stdout.

--- power_to_the_people.rb
+++ power_to_the_people.rb
@@ -1,36 +1,55 @@
-class PeoplePowerCalculator
-  def initialize(number)
-    @number = number
+module PowerToThePeople
+  class Presenter
+    def initialize(count = 100)
+      @count = 100
+    end
+
+    def self.present
+      new.to_stdout
+    end
+
+    def to_stdout
+      [*1..count].each do |num|
+        calc = Calculator.new(num)
+
+        if calc.people_power?
+          puts "People Power"
+        elsif calc.people?
+          puts "People"
+        elsif calc.power?
+          puts "Power"
+        else
+          puts num
+        end
+      end
+    end
+
+    private
+
+    attr_reader :count
   end
 
-  def people?
-    number%3 == 0
+  class Calculator
+    def initialize(number)
+      @number = number
+    end
+
+    def people?
+      number%3 == 0
+    end
+
+    def power?
+      number%5 == 0
+    end
+
+    def people_power?
+      people? && power?
+    end
+
+    private
+
+    attr_reader :number
   end
-
-  def power?
-    number%5 == 0
-  end
-
-  def people_power?
-    people? && power?
-  end
-
-  private
-
-  attr_reader :number
 end
 
-
-[*1..100].each do |num|
-  calc = PeoplePowerCalculator.new(num)
-
-  if calc.people_power?
-    puts "#{num} People Power"
-  elsif calc.people?
-    puts "#{num} People"
-  elsif calc.power?
-    puts "Power"
-  else
-    puts num
-  end
-end
+PowerToThePeople::Presenter.present
